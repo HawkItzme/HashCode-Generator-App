6cfef8be67fa2478707f997e1b7d26cff9527d88
Hawk Itzme <smquasim016@gmail.com>
2023-01-08 16:34:50 +0530

    Animation before Success Fragment code added

--- app/src/main/java/com/example/hashcodegenerator/HomeFragment.kt
+++ app/src/main/java/com/example/hashcodegenerator/HomeFragment.kt
@@ -1,10 +1,15 @@
 package com.example.hashcodegenerator
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.ArrayAdapter
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.hashcodegenerator.databinding.FragmentHomeBinding
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
@@ -22,11 +27,46 @@ class HomeFragment : Fragment() {
         val hashAlgorithms = resources.getStringArray(R.array.hash_algorithms)
         val arrayAdapter = ArrayAdapter(requireContext(), R.layout.drop_down_item, hashAlgorithms)
         binding.autoCompleteTV.setAdapter(arrayAdapter)
+
+        binding.genBtn.setOnClickListener{
+            lifecycleScope.launch{
+                applyAnimations()
+                navigateToSuccess()
+            }
+        }
+
         return binding.root
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.home_menu, menu)
+    }
+
+    private suspend fun applyAnimations(){
+        binding.titleTextView.animate().alpha(0f).duration = 400L
+        binding.genBtn.animate().alpha(0f).duration = 400L
+        binding.textInputLayout.animate()
+            .alpha(0f)
+            .translationXBy(1200f)
+            .duration = 400L
+        binding.plainText.animate().alpha(0f).translationXBy(-1200f)
+            .duration = 400L
+
+        delay(300)
+
+        binding.successBackground.animate().alpha(1f).duration = 600L
+        binding.successBackground.animate().rotationBy(720f).duration = 600L
+        binding.successBackground.animate().scaleXBy(900f).duration = 800L
+        binding.successBackground.animate().scaleYBy(900f).duration = 800L
+
+        delay(300L)
+        binding.successIV.animate().alpha(1f).duration = 1000L
+
+        delay(1500L)
+    }
+
+    private fun navigateToSuccess(){
+        findNavController().navigate(R.id.action_homeFragment_to_successFragment)
     }
 
     override fun onDestroy() {
